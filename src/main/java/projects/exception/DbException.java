1e49ca1d957a3020ac82bd715eb3b2ecabe1ae70
Edith Torres <tedith56@yahoo.com>
2025-01-31 11:35:57 -0700

    commit

---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,26 @@
+package projects.exception;
+
+
+public class DbException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DbException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+}
